c1b2fe40a2a26c41fda651c0eea5ca0ad12a0372
Dimitrios Chr. Ioannidis <d.ioannidis@nephelae.eu>
2020-03-09 16:56:21 +0200

    Misc.

--- demo/ufp_i2c_twi.pas
+++ demo/ufp_i2c_twi.pas
@@ -1,4 +1,5 @@
 unit ufp_i2c_twi;
+
 {
 
   SPI Protocol.
@@ -23,15 +24,20 @@ unit ufp_i2c_twi;
 
 }
 
-{$mode objfpc}{$H+}
+{$mode objfpc}
+{$LONGSTRINGS OFF}
+{$INLINE ON}
+{$MACRO ON}
+{$MODESWITCH TYPEHELPERS}
+{$MODESWITCH ADVANCEDRECORDS}
 {$WRITEABLECONST OFF}
 
 interface
 
 const
-  I2C_Takt  = 100000;
+  I2C_Takt = 100000;
   TWI_Write = 0;
-  TWI_Read  = 1;
+  TWI_Read = 1;
   SDA = 4;
   SCL = 5;
 
@@ -54,8 +60,8 @@ begin
   TWSR := 0;
   TWBR := byte(TWBR_val);
 
-  PORTC := PORTC or ( 1 shl SDA); // Pullup Register SDA
-  PORTC := PORTC or ( 1 shl SCL); // Pullup Register SCL
+  PORTC := PORTC or (1 shl SDA); // Pullup Register SDA
+  PORTC := PORTC or (1 shl SCL); // Pullup Register SCL
 
   TWSR := TWSR and not (1 shl TWPS);
   TWSR := TWSR and not (1 shl 1);
@@ -65,12 +71,14 @@ procedure TWIStart(const addr: byte);
 begin
   TWCR := 0;
   TWCR := (1 shl TWINT) or (1 shl TWSTA) or (1 shl TWEN);
-  while ((TWCR and (1 shl TWINT)) = 0) do begin
+  while ((TWCR and (1 shl TWINT)) = 0) do
+  begin
   end;
 
   TWDR := addr;
   TWCR := (1 shl TWINT) or (1 shl TWEN);
-  while ((TWCR and (1 shl TWINT)) = 0) do begin
+  while ((TWCR and (1 shl TWINT)) = 0) do
+  begin
   end;
 end;
 
@@ -83,14 +91,16 @@ procedure TWIWrite(const u8data: byte);
 begin
   TWDR := u8data;
   TWCR := (1 shl TWINT) or (1 shl TWEN);
-  while ((TWCR and (1 shl TWINT)) = 0) do begin
+  while ((TWCR and (1 shl TWINT)) = 0) do
+  begin
   end;
 end;
 
 function TWIReadACK: byte;
 begin
   TWCR := (1 shl TWINT) or (1 shl TWEN) or (1 shl TWEA);
-  while (TWCR and (1 shl TWINT)) = 0 do begin
+  while (TWCR and (1 shl TWINT)) = 0 do
+  begin
   end;
   Result := TWDR;
 end;
@@ -99,42 +109,50 @@ end;
 function TWIReadNACK: byte;
 begin
   TWCR := (1 shl TWINT) or (1 shl TWEN);
-  while (TWCR and (1 shl TWINT)) = 0 do begin
+  while (TWCR and (1 shl TWINT)) = 0 do
+  begin
   end;
   Result := TWDR;
 end;
 
 function TWIReadACK_Error: byte;
 var
-  err:byte;
+  err: byte;
 begin
   err := 0;
   TWCR := (1 shl TWINT) or (1 shl TWEN) or (1 shl TWEA);
-  while ((TWCR and (1 shl TWINT)) = 0) and (err < 255) do begin
+  while ((TWCR and (1 shl TWINT)) = 0) and (err < 255) do
+  begin
     Inc(err);
   end;
-  if err = 255 then begin
+  if err = 255 then
+  begin
     Result := 0;
-  end else begin
+  end
+  else
+  begin
     Result := TWDR;
   end;
 end;
 
 function TWIReadNACK_Error: byte;
 var
-  err:byte;
+  err: byte;
 begin
   err := 0;
   TWCR := (1 shl TWINT) or (1 shl TWEN);
-  while ((TWCR and (1 shl TWINT)) = 0) and (err < 255) do begin
+  while ((TWCR and (1 shl TWINT)) = 0) and (err < 255) do
+  begin
     Inc(err);
   end;
-  if err = 255 then begin
+  if err = 255 then
+  begin
     Result := 0;
-  end else begin
+  end
+  else
+  begin
     Result := TWDR;
   end;
 end;
 
 end.
-
